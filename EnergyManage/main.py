e911e98e4573cd072f1348ed6ccbafd8e4da0149
real_company <186612857@qq.com>
2023-09-01 20:37:22 +0800

    最终完成版本，主要更新定时触发脚本功能并自动打开检查报告

--- EnergyManage/main.py
+++ EnergyManage/main.py
@@ -4,11 +4,14 @@ import requests
 from Data import *  # 导入Data中的方法
 import datetime
 import os
+import time
+import pyautogui
+import schedule
 
 headers = {'Content-Type': 'application/json;charset=UTF-8'}
 response = requests.post(ApiUrl.login_url, data=JsonFile.json_login, headers=headers)
 token_data = 'Bearer ' + response.json()['token']
-report_path = os.getcwd() + os.sep + 'report' + os.sep + '{}巡检报告.txt'.format(datetime.date.today())
+report_path = os.getcwd() + os.sep + 'report' + os.sep + '{}-report.txt'.format(datetime.date.today())
 
 
 class KunShangProject(object):
@@ -24,8 +27,6 @@ class KunShangProject(object):
                                            data=JsonFile.json_day_total_energy, headers=headers_check)
             x = response_check.json()
             return x['data']['total']
-
-        # daily_energy_total = '今日总计用能：' + str(x['data']['total']) + x['data']['dimension']
 
         # 获取分时模式下各时段值
         if way == 2:
@@ -55,21 +56,15 @@ class KunShangProject(object):
         times_energy_total = KunShangProject.daily_check_get_data(2)
         curtimes = times_energy_total['curTimes']
         current_time = datetime.datetime.now().hour + 1
-        print(curtimes)
-        print(len(curtimes))
-        print(current_time)
 
         if len(curtimes) == current_time:
-            print('44444444444444444444')
             result = '分时能耗异常 \n' \
                      '当前存在分时能耗数：{} \n'.format(len(curtimes))
             return result
         elif len(curtimes) == 0:
-            print('111111111111111111')
             result3 = 0
             return result3
         else:
-            print('33333333333333')
             result1 = []
             pass_count = 0
             for i in range(len(curtimes)):
@@ -93,8 +88,6 @@ class KunShangProject(object):
         report_context1 = '分时能耗正常，和小时详细能耗如下： \n' \
                           '第{}小时正常，正常值为{}kw'
 
-        print("times_energy_analysis() type:", type(KunShangProject.times_energy_analysis()))
-
         if isinstance(KunShangProject.times_energy_analysis(), list):
             print("Entering the list branch")  # 添加调试输出
 
@@ -107,23 +100,36 @@ class KunShangProject(object):
             print("Entering the string branch")  # 添加调试输出
             times_energy_total = KunShangProject.daily_check_get_data(2)
             curtimes = times_energy_total['curTimes']
-
             for i in range(len(curtimes)):
                 for key, value in curtimes[i].items():
                     report_context1 = report_context1.format(key, value)
                     return report_context1
+
         elif isinstance(KunShangProject.times_energy_analysis(), int):
-            print('000000000000000000000000000')
             report_context2 = '分时能耗异常： \n' \
                               '所有分时能耗都为0'
             return report_context2
 
     @staticmethod
-    def putout_report():
+    def putout_report():  # 输出报告
         with open(report_path, 'w') as my_report:
             my_report.write('{} \n'.format(KunShangProject.daily_check_data_analysis()))
             my_report.write(KunShangProject.edit_report())
 
+        # 输出完报告后自动打开报告
+        time.sleep(10)
+        pyautogui.hotkey('win', 'r')
+        time.sleep(2)
+        secs_between_keys = 0.1
+        pyautogui.typewrite(message=report_path, interval=secs_between_keys)
+        time.sleep(1)
+        pyautogui.hotkey('enter')
+        pyautogui.hotkey('enter')
+
 
 if __name__ == "__main__":
-    KunShangProject.putout_report()
+    schedule.every().day.at("06:00:00").do(KunShangProject.putout_report)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
